c31065341924de4486c5499fc7917f7e1b21dced
Wesley Bond <wesley.bond@icloud.com>
2017-06-15 09:59:25 -0400

    bank

---
+++ bankaccount.rb
@@ -0,0 +1,29 @@
+class BankAccount
+  attr_accessor :name, :balance
+  def initialize(name, balance=0)
+    @name = name
+    if balance >= 100
+      @balance = balance
+      else
+        raise ArgumentError, "Minimum of $100 not met"
+    end
+  end
+  def balance
+    return @balance
+  end
+  def add(deposit)
+    return @balance += deposit
+  end
+  def subtract(withdrawal)
+    return @balance -= withdrawal
+  end
+  def transfer(amount,account)
+    if amount <= 0
+      raise ArgumentError, "Transfer must be more than zero"
+    else
+    @balance -= amount
+    end
+    account.balance += amount
+    "Transaction completed"
+  end
+end
